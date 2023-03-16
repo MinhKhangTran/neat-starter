d274f6efe2edaa2e43b83ea1ed7d24093a429e45
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2023-03-16 12:42:15 +0100

    :bug: debugg

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 const items = require('../src/_data/price.json')
 
 exports.handler = async (event, context, cb) => {
+    console.log(items)
     return {
       headers: {
         'Access-Control-Allow-Origin': '*',
