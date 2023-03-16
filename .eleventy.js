8330e47feacf07600a2a0c166188b0c4f775c8a3
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2023-03-16 11:40:44 +0100

    :tada: passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,9 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+   // Copy json to route of /_site
+   eleventyConfig.addPassthroughCopy("./src/_data/price.json");
+
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
